ff62c1c2e6cc6f4b3e8632f0298d3383fdbce518
Abhinav Arora <abhinavaroraa14799@gmail.com>
2021-12-09 19:07:58 -0800

    deployment issues fixed

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 require("dotenv").config();
 
 const express = require("express");
-const passport = require("passport");
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
+// const passport = require("passport");
+// const session = require("express-session");
+// const MySQLStore = require("express-mysql-session")(session);
 
-const db = require("./models/db");
-const localStrategy = require("./utils/authStrategy").localStrategy;
+// const db = require("./models/db");
+// const localStrategy = require("./utils/authStrategy").localStrategy;
 
 // ---------------------- GENERAL SETUP ----------------------
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // ---------------------- SESSION STORE SETUP ----------------------
-const sessionStore = new MySQLStore({}, db);
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        store: sessionStore,
-        cookie: {
-            maxAge: 1000 * 60 * 60, // 1 hours cookie expiration date
-        },
-    })
-);
+// const sessionStore = new MySQLStore({}, db);
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET_KEY,
+//         resave: false,
+//         saveUninitialized: false,
+//         store: sessionStore,
+//         cookie: {
+//             maxAge: 1000 * 60 * 60, // 1 hours cookie expiration date
+//         },
+//     })
+// );
 
 // ---------------------- PASSPORT CONFIGURATION ----------------------
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(localStrategy);
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(localStrategy);
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//     done(null, user);
+// });
 
-passport.deserializeUser((user, done) => {
-    db.query("SELECT * FROM user WHERE id=?", [user.id], (err, result) => {
-        if (err) {
-            done(err);
-            return;
-        }
-        user = result[0];
-        done(err, user);
-    });
-});
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
+// passport.deserializeUser((user, done) => {
+//     db.query("SELECT * FROM user WHERE id=?", [user.id], (err, result) => {
+//         if (err) {
+//             done(err);
+//             return;
+//         }
+//         user = result[0];
+//         done(err, user);
+//     });
+// });
+// app.use((req, res, next) => {
+//     res.locals.user = req.user;
+//     next();
+// });
 // ---------------------- ROUTES ----------------------
 const userRoutes = require("./routes/users");
 const APIRoutes = require("./routes/api");
--- models/db.js
+++ models/db.js
@@ -1,16 +1,16 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-connectionOptions = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-};
+// connectionOptions = {
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+// };
 
-const db = mysql.createConnection(connectionOptions);
-db.connect((err) => {
-    if (err) throw err;
-    console.log("Established connection with database");
-});
+// const db = mysql.createConnection(connectionOptions);
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log("Established connection with database");
+// });
 
-module.exports = db;
+// module.exports = db;
